ec93b054330be3c4fc1dcce4f71bc3682a4793fd
belharche <brahimelharche02@gmail.com>
2026-01-19 16:10:05 +0100

    locking up fetched resource to achieve concurrency and avoid race condition or resevation overlap

--- src/main/java/arimayichallenge/repository/ResourceRepository.java
+++ src/main/java/arimayichallenge/repository/ResourceRepository.java
@@ -1,7 +1,17 @@
 package arimayichallenge.repository;
 
 import arimayichallenge.domain.resource.Resource;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select r from Resource r where r.id = :id")
+    Optional<Resource> findByIdForUpdate(Long id);
+
 }
