548e3568fca1cb71df5c6cf40b593d8c79dedc15
belharche <brahimelharche02@gmail.com>
2026-01-19 18:56:33 +0100

    add the main class

---
+++ src/main/java/arimayichallenge/AriMayiChallengeApplication.java
@@ -0,0 +1,13 @@
+package arimayichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriMayiChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AriMayiChallengeApplication.class, args);
+    }
+
+}
